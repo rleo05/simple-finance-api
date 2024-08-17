29b1602e4ec31220b5f2190aecdf56c6cf5d75ea
Leonardo <leorod2005@gmail.com>
2024-08-17 18:22:15 -0300

    initial commit

---
+++ src/main/java/com/project/simple_finance_api/SimpleFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.simple_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/simple_finance_api/SimpleFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simple_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
